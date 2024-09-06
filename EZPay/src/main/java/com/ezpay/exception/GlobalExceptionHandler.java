5ff6c1149db5a947d811fa02257121b7d79276f2
Shivaji Reddy Suram <shivajireddy980@gmail.com>
2024-09-06 14:30:21 +0000

    Added Transaction status usecase

---
+++ EZPay/src/main/java/com/ezpay/exception/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+/**
+ * Author: Shivaji Reddy Suram
+ * Date: 30/08/2024
+ */
+package com.ezpay.exception;
+
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+//import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * Handles TransactionNotFoundException and returns a custom error response.
+     * 
+     * @param ex The exception that was thrown.
+     * @return ResponseEntity with error message and status code.
+     */
+    @ExceptionHandler(TransactionNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorResponse> handleTransactionNotFoundException(TransactionNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Handles any other exceptions and returns a generic error response.
+     * 
+     * @param ex The exception that was thrown.
+     * @return ResponseEntity with error message and status code.
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An unexpected error occurred.");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
