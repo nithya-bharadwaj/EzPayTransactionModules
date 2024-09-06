97e689a55e7a83f180360e13179508d6aabc65df
Shivaji Reddy Suram <shivajireddy980@gmail.com>
2024-09-06 14:32:28 +0000

    Added Transaction status usecase

---
+++ EZPay/src/main/java/com/ezpay/service/TransactionStatusServiceInterface.java
@@ -0,0 +1,51 @@
+package com.ezpay.service;
+
+/**
+ * Author: Shivaji Reddy Suram
+ * Date: 30/08/2024
+ */
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+
+import com.ezpay.entity.Transaction;
+
+/**
+ * Interface defining the contract for transaction-related operations.
+ * Provides methods to manage transactions, including adding, tracking status, 
+ * retrieving history, and fetching transactions by ID.
+ */
+public interface TransactionStatusServiceInterface {
+	/**
+     * Adds a new transaction to the repository.
+     * 
+     * @param transaction The transaction object to be added.
+     * @return The added transaction object.
+     */
+	public Transaction addTransactionService(Transaction transaction);
+	 /**
+     * Tracks the status of a transaction based on the transaction ID.
+     * 
+     * @param transactionId The ID of the transaction to be tracked.
+     * @return The status of the transaction.
+     * @throws Exception If the transaction status cannot be found.
+     */
+	public String trackTransactionStatus(int transactionId) throws Exception;
+	/**
+     * Retrieves the history of all transactions.
+     * 
+     * @return A list of all transactions.
+     */
+	public List<Transaction> getTransactionHistory();
+	/**
+     * Retrieves a transaction by its ID.
+     * 
+     * @param transactionId The ID of the transaction to be retrieved.
+     * @return An Optional containing the transaction if found, otherwise empty.
+     */
+	public Optional<Transaction> getTransactionById(int transactionId);
+}
+
