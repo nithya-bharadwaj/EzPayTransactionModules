0cb60bbe4238c82366db9f4a5cdab85429020fe2
Preethi R <110417264+rpreethi-r@users.noreply.github.com>
2024-09-20 12:07:33 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,50 @@
-import { Link } from "react-router-dom";
-import "../styles/Home.css"; // Import the updated CSS file for styling
-import logo from '../assets/EzpayLogo.png'; // Add your logo file here
+/**
+ * Author: Preethi R
+ * Date: 20/09/2024
+ * 
+ * Description:
+ * This component defines the Navbar for the EzPay application, providing navigation links
+ * to key pages like Home, About Us, Contact Us, Transaction History, and Scheduled Payments.
+ */
+
+import React from "react";
+import { NavLink } from "react-router-dom"; 
+import "./Navbar.css";
+import logo from './assets/EzPayLogo.png'; 
+
 const Navbar = () => {
-	return (
-		<nav className="navbar navbar-expand-lg">
-			<div className="container-fluid">
-				<Link className="navbar-brand" to="/">
-					<img src={logo} alt="EzPay Logo" className="logo" />
-					<span className="navbar-title">EzPay</span>
-				</Link>
-				<div className="collapse navbar-collapse" id="navbarNav">
-					<ul className="navbar-nav ms-auto">
-						<li className="nav-item">
-							<Link className="nav-link" to="/">Home</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="/about">About Us</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="/contact">Contact Us</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="/transactions">Transaction History</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="/schedule">Manage Payments</Link>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	);
-}
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        {/* Logo and brand title */}
+        <NavLink className="navbar-brand" to="/">
+          <img src={logo} alt="EzPay Logo" className="logo" />
+          <span className="navbar-title" style={{ color: '#a5ffd6' }}>EzPay</span>
+        </NavLink>
+
+        {/* Navbar links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/" activeClassName="active">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about" activeClassName="active">About Us</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/contact" activeClassName="active">Contact Us</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/transactions" activeClassName="active">Transaction History</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/schedule" activeClassName="active">Scheduled Payments</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
